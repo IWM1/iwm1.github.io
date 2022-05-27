545f78775d40b30361e602527eb8b0830daad72a
IWM1 <iwmitiku@gmail.com>
2022-05-27 16:54:16 -0500

    edits

--- BasicJavaScript/firstJSAssignment.js
+++ BasicJavaScript/firstJSAssignment.js
@@ -1,5 +1,3 @@
-
-
 //Question1
 //Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
 function question1(number1, number2){
